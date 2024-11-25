39dcd8aefea0422a51cb75237a89e9c5de7c171e
oliver po <oliver.po2882@gmail.com>
2024-11-25 21:26:07 +1000

    split into stages for boot.asm

--- bootloader/src/stage0.asm
+++ bootloader/src/stage0.asm
@@ -1,7 +1,34 @@
-[org 0x7c00]
 [bits 16]
+; Initialise the stack
 
-	hlt
+	mov bp, 0x7c00
+	mov sp, bp
 
-times	510 - ($ - $$) db 0
-dw	0xAA55
+
+	mov bx, success_stack_msg
+	call print
+
+	mov bx, 0x7e00
+	mov dh, 1
+	call disk_load
+
+	call enable_a20
+
+	mov bx, success_a20_msg
+	call print
+
+	call init_vga
+
+	call switch_to_pm
+
+	jmp CODE_SEG:init_pm
+
+success_stack_msg: db "Stack is initialised", 13, 10, 0
+success_a20_msg: db "a20 successful", 30, 10, 0
+
+; Imports for sector 1
+
+%include "io.asm"
+
+times 510-($-$$) db 0
+dw 0xAA55

f04972df6daa58dd6b1c3ae76831ee71375e7441
Oliver Po <z5593381@ad.unsw.edu.au>
2024-12-02 22:36:07 +0900

    BROKEN: replace load_disk with load_disk_lba

--- bootloader/src/stage0.asm
+++ bootloader/src/stage0.asm
@@ -8,9 +8,12 @@
 	mov	bx,	success_stack_msg
 	call	print
 
-	mov	bx,	0x7e00
-	mov	dh,	1
-	call	disk_load
+;	mov	bx,	0x7e00
+;	mov	dh,	1
+;	call	disk_load
+
+	mov	si, DAPACK
+	call	disk_load_lba
 
 	call	enable_a20
 
@@ -25,6 +28,16 @@
 
 success_stack_msg: db "Stack is initialised", 13, 10, 0
 success_a20_msg: db "a20 successful", 30, 10, 0
+
+DAPACK:
+	db	0x10
+	db	0
+; 	Read one sector
+blkcnt:	dw	1
+db_add:	dw	0x7e00
+d_lba:	dd	1
+	dd	0
+
 
 ; Imports for sector 1
 

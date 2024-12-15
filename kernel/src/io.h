940c7f2c6b353431c9b6c89cd3af040c8b506928
Oliver Po <z5593381@ad.unsw.edu.au>
2024-12-15 22:11:00 +0400

    add documentation to io.h

--- kernel/src/io.h
+++ kernel/src/io.h
@@ -1,7 +1,22 @@
 #pragma once
 
+// Prints the integer given to the terminal
 void puti(int num);
+
+// Prints the character given to the terminal
 void putc(const char c);
+
+// Prints the string given to the terminal
 void puts(const char *s);
+
+// Prints the double given to the terminal
 void putd(const double f);
+
+/* Prints the formatted string given to the terminal
+ * format specifiers:
+ * %s - string
+ * %c - character
+ * %d - integer
+ * %f - double
+ */
 void krintf(const char *format, ...);

48258245647e91a93ae9b93ea23803417cdefddf
Oliver Po <oliver.po2882@gmail.com>
2025-01-01 16:51:25 +1000

    setup functions for idt

--- kernel/src/arch/x86/cpu/idt.c
+++ kernel/src/arch/x86/cpu/idt.c
@@ -1,17 +1,37 @@
 #include "idt.h"
 
-struct idt_descriptor interrupt_gate(enum gate_type type, uint8_t privilege_level, uint16_t segment_selector) {
-    struct idt_descriptor idt_descriptor = { 0 };
+struct interrupt_descriptor interrupt_descriptors[256] = { 0 };
+
+struct idt_descriptor idt_descriptor = {
+    .idt_size = sizeof(interrupt_descriptors),
+    .idt_start = &interrupt_descriptors
+};
+
+void setup_interrupt_gate(uint32_t irq, uint32_t base, enum gate_type type, uint8_t privilege_level, uint16_t segment_selector) {
+    struct interrupt_descriptor *interrupt_descriptor = &interrupt_descriptors[irq];
 
     // present bit must be set for descriptor to be valid
-    idt_descriptor.type_attributes |= 1 << 7;
+    interrupt_descriptor->type_attributes |= 1 << 7;
 
     // privilege level is only 2 bits, we bitwise AND just in case
-    idt_descriptor.type_attributes |= (privilege_level & 0x3) << 5;
+    interrupt_descriptor->type_attributes |= (privilege_level & 0x3) << 5;
 
-    idt_descriptor.type_attributes |= type;
+    interrupt_descriptor->type_attributes |= type;
     
-    idt_descriptor.selector = segment_selector;
+    interrupt_descriptor->selector = segment_selector;
 
-    return idt_descriptor;
+    // address of the routine
+    interrupt_descriptor->offset_1 = base & 0xFFFF;
+    interrupt_descriptor->offset_2 = (base >> 16) & 0xFFFF;
+}
+
+void load_idt(void) {
+    asm volatile ("lidt %0" :: "m"(idt_descriptor));
+}
+
+uint16_t idt_segment_descriptor(uint16_t index) {
+    // index takes up 12 bits
+    // RPL = 0
+    // Table indicator = 0 (GDT)
+    return ((index & 0xFFF) << 3);
 }
--- kernel/src/arch/x86/cpu/idt.h
+++ kernel/src/arch/x86/cpu/idt.h
@@ -3,13 +3,18 @@
 #include <stdint.h>
 
 // Refer to: https://wiki.osdev.org/Interrupt_Descriptor_Table
-struct idt_descriptor {
+struct interrupt_descriptor {
     uint16_t offset_1;
     uint16_t selector;
     uint8_t zero;
     uint8_t type_attributes;
     uint16_t offset_2;
-};
+} __attribute__((packed));
+
+struct idt_descriptor {
+    uint16_t idt_size;
+    uint32_t idt_start;
+} __attribute__((packed));
 
 enum gate_type {
     TASK_GATE = 0x5,
@@ -18,3 +23,9 @@ enum gate_type {
     INTERRUPT_32_GATE = 0xE,
     TRAP_32_GATE = 0xF
 };
+
+void setup_interrupt_gate(uint32_t irq, enum gate_type type, uint8_t privilege_level, uint16_t segment_selector);
+
+void load_idt(void);
+
+uint16_t idt_segment_descriptor(uint16_t index);

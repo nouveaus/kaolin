7152b6e41eca28ac575f0eceb58de63d2187e700
Ramid Khan <ramidzkh@gmail.com>
2025-01-07 13:28:03 +1100

    Remove unnecessary const

--- kernel/src/arch/x86/apic/ioapic.h
+++ kernel/src/arch/x86/apic/ioapic.h
@@ -5,10 +5,10 @@
 #define IOAPICBASE 0xFEC00000
 
 // Reads register from ioapic
-uint32_t ioapic_read_reg(void *ioapicaddr, const uint32_t reg);
+uint32_t ioapic_read_reg(void *ioapicaddr, uint32_t reg);
 
 // Writes register to ioapic
-void ioapic_write_reg(void *ioapicaddr, const uint32_t reg, const uint32_t value);
+void ioapic_write_reg(void *ioapicaddr, uint32_t reg, uint32_t value);
 
 // Redirect a hardware interrupt to a local apic
-void ioapic_set_redirect(void *ioapicaddr, const uint8_t irq, const uint8_t vector, const uint8_t apic_id);
+void ioapic_set_redirect(void *ioapicaddr, uint8_t irq, uint8_t vector, uint8_t apic_id);
--- kernel/src/arch/x86/serial/serial.h
+++ kernel/src/arch/x86/serial/serial.h
@@ -6,7 +6,7 @@
 #define STATUS_PORT 0x64
 
 // Writes a byte to the serial port
-void outb(const uint16_t port,  const uint8_t value);
+void outb(uint16_t port, uint8_t value);
 
 // Reads a byte from the serial port
-uint8_t inb(const uint16_t port);
+uint8_t inb(uint16_t port);

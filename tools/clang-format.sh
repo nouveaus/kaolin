a214e7fdb6e4d8bc505e03ee64fd4517444b742a
Ramid Khan <ramidzkh@gmail.com>
2025-01-17 14:15:59 +1100

    Add clang-format to build

---
+++ tools/clang-format.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if ! command -v "${CLANG_FORMAT}" &>/dev/null; then
+  CLANG_FORMAT=clang-format-19
+fi
+
+if ! command -v "${CLANG_FORMAT}" &>/dev/null; then
+  CLANG_FORMAT=clang-format-18
+fi
+
+if ! command -v "${CLANG_FORMAT}" &>/dev/null; then
+  CLANG_FORMAT=clang-format
+fi
+
+find kernel/src/ \( -iname '*.h' -o -iname '*.c' \) -print0 | xargs -0 "${CLANG_FORMAT}" "$@" -i

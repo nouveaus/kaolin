650325a663821945e2b2b0af6303e27dc37524c3
Ramid Khan <ramidzkh@gmail.com>
2025-01-26 11:07:01 +1100

    Split linking into ELF and binary stage

--- tools/bind_kernel.py
+++ tools/bind_kernel.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import struct
+import subprocess
 
 def pad_to_sector(file, size):
     position = file.tell()
@@ -16,11 +17,21 @@ def main():
     parser.add_argument('--kernel', required=True, help="Path of the kernel image")
     parser.add_argument('--entry', required=True, help="Base and entry address")
     parser.add_argument('--output', required=True, help="Path to the output file")
+    parser.add_argument('--cc', required=True, help="Compiler driver")
+    parser.add_argument('--ls', required=True, help="Linker script")
     parser.add_argument('--align', help="Sector size for alignment", default=0x200, type=int)
     args = parser.parse_args()
 
     if isinstance(args.entry, str):
         args.entry = int(args.entry, 0)
+
+    try:
+        os.remove("/tmp/kaolin.bin")
+    except:
+        pass
+
+    subprocess.run([args.cc, "-nostdlib", "-T", args.ls, args.kernel, "-o", "/tmp/kaolin.bin"])
+    args.kernel = "/tmp/kaolin.bin"
 
     with open(args.output, 'wb') as output:
         # write bootloader
